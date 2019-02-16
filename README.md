233deccfd6f3c2971fa9fead972a4c85f5d93f9b
Emmanuel Béziat <contact@emmanuelbeziat.com>
2019-02-16 04:17:19 +0100

    spotify

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# get-spotify-id
+# get-audio-id
 JS tool to parse spotify url to get a song ID.
+
+> *Important Note:* This work is a fork-ish of [https://github.com/radiovisual/get-video-id](get-video-id) from @radiovisual, but meant to use audio services (Spotify, Deezer, etc.). All credits for the work should go to him!
+
+This module will extract the Spotify, Deezer, including embed strings.
+
+# Install
+
+```bash
+$ npm install --save get-audio-id
+```
+
+# Usage
+
+Just call the function and give it a spotify url (or embed string). Its metadata (id and service) will be extracted.
+
+```javascript
+const getAudioId = require('get-audio-id)
+
+getAudioId('https://open.spotify.com/track/2LfMWfjk9wIGv9sGTjhg85')
+// => {id: '2LfMWfjk9wIGv9sGTjhg85', service: 'spotify' }
+```
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict'
+var getSrc = require('get-src')
+
+module.exports = function (str) {
+	if (typeof str !== 'string') {
+		throw new typeError('get-spotify-id expects a string')
+	}
+
+	if (/<iframe/ig.test(str)) {
+		str = getStr(str)
+	}
+
+	// Remove surrounding whitespaces or linefeeds
+	str = str.trim()
+
+	var metadata = {}
+
+	// Try to handle google redirection uri
+	if (/\/\/google/.test(str)) {
+		// Find the redirection uri
+		var matches = str.match(/url=([^&]+)&/)
+
+		if (matches) {
+			str = decodeURIComponent(matches[1])
+		}
+	}
+
+	if (/spotify/.test(str)) {
+		metadata = {
+			id: spotify(str),
+			service: 'spotify'
+		}
+	}
+
+	return metadata
+}
+
+/**
+ * Get the Spotify ID
+ * @param str { string } the url from wich you want to extract the id
+ * @returns { string | undefined }
+ */
+function spotify (str) {
+	var open = /https:\/\/open\.spotify\.com\/track\//gmi
+	var url = /spotify:track:/gmi
+
+	if (open.test(str)) {
+		return str.split(open)[1]
+	}
+
+	if (url.test(str)) {
+		return str.split(url)[1]
+	}
+}
